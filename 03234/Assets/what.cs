96b216671b11c932eed4e1a77f51d7c2da635fa7
Simon <mmm998520@gmail.com>
2020-06-13 15:14:32 +0800

    你要做指甲花紋

---
+++ 03234/Assets/what.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class what : MonoBehaviour
+{
+    
+	public Animator animator;
+	public GameObject Dragon;
+
+    void Start()
+    {
+      animator = GetComponent<Animator>();  
+    }
+
+    
+    void Update()
+    {
+	  animator.speed =1.0f;
+	}
+
+	public void sss(){
+	animator = GetComponent<Animator>();
+	animator.speed =0.0f;
+	}
+	
+	public void ooo(){
+	animator = GetComponent<Animator>();
+	animator.speed =1.0f;
+	}
+	
+	public void www(){
+	Dragon.SetActive(true);
+	}
+	public void ddd(){
+	Dragon.SetActive(false);
+	}
+}
